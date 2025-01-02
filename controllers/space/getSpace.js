1a2f66ee602dfebbda1f88cd2a23c4c11761c53d
Shoyeb Ashkan <shoyebashkan999@gmail.com>
2025-01-02 11:30:39 +0530

    some edge case for email

--- controllers/space/getSpace.js
+++ controllers/space/getSpace.js
@@ -3,23 +3,24 @@ const Space = require("../../models/Space");
 const getSpace = async (req, res) => {
   try {
     const spaceId = req.params.spaceId;
-    // Fetch the space details
     const getSpace = await Space.findById(spaceId)
       .select("-__v")
       .populate({
         path: "rootFolder",
         options: { sort: { createdAt: -1 } },
         select: "-__v -updatedAt",
-        populate: {
-          path: "children",
-          options: { sort: { createdAt: -1 } },
-          select: "-__v -updatedAt",
-        },
-        populate: {
-          path: "responses",
-          options: { sort: { submittedAt: -1 } },
-          select: "-__v -updatedAt",
-        },
+        populate: [
+          {
+            path: "children",
+            options: { sort: { createdAt: -1 } },
+            select: "-__v -updatedAt",
+          },
+          {
+            path: "responses",
+            options: { sort: { submittedAt: -1 } },
+            select: "-__v -updatedAt",
+          },
+        ],
       });
 
     if (!getSpace) {
